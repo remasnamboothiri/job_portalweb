5442c218dff9eaafe2de31eef6350d3e2d883e5b
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2025-08-26 10:55:03 +0530

    fix the media files

--- job_platform/urls.py
+++ job_platform/urls.py
@@ -46,3 +46,9 @@ if settings.DEBUG:
 if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
     
+    
+    
+# Add this for serving media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
+    

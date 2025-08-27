de9924e5ed6e35e8f8fab0839639558c1c93f4d6
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2025-08-27 13:06:46 +0530

    fix the building issue

--- jobapp/forms.py
+++ jobapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser, Profile, Job, Application , Interview
+from .models import CustomUser, Profile, Job, Application , Interview , Candidate
     
 
 

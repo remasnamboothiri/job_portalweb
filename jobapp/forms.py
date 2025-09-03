8499444ea44f4298be23a187d8ca076481e132ce
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2025-09-03 11:39:25 +0530

    remove the top import section from forms.py

--- jobapp/forms.py
+++ jobapp/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser, Profile, Job, Application , Interview , Candidate
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
+
     
 
 
